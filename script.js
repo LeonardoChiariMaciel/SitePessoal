344fa552b3f76374d9928993a6bcca16d13340ec
Leonardo <leo@lithera.com.br>
2025-03-17 19:17:21 -0300

    Modificacoes finais

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("Message sent successfully!");
     });
 });
+
+document.getElementById("continue").addEventListener("click", function (event) {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    document.querySelector("#about").scrollIntoView({
+        behavior: "smooth", // Ativa a rolagem suave
+        block: "start" // Garante que a seção comece no topo da visualização
+    });
+});
